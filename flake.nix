877ab243462432af9d50361644beb6ddc2bd8a28
albertonoys <albertonoys@gmail.com>
2024-10-12 22:02:07 +0200

    Add flake, lefthook and direnv

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    in
+    {
+      packages = forAllSystems (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          clang-tools = pkgs.clang-tools_18;
+          lefthook = pkgs.lefthook;
+        });
+
+      devShells = forAllSystems (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            buildInputs = [
+              pkgs.clang-tools_18
+              pkgs.lefthook
+            ];
+          };
+        });
+    };
+}
